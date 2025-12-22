6c465e6463137bc7cd83970e47db51b06c11409a
ratworld <jayvoigt0@gmail.com>
2025-12-21 20:03:25 -0500

    adjust ui

--- addons/Tea/src/tea_cfg.gd
+++ addons/Tea/src/tea_cfg.gd
@@ -1,12 +1,14 @@
 class_name TeaCfg extends Resource
 
-@export_group("Behavior", "behavior_")
-@export var behavior_stop_when_source_dies := false
-@export var behavior_stationary := false
+@export_group("Behavior")
+@export var stop_when_source_dies := false
+@export var stationary := false
 
-@export_group("Global playback", "playback_")
-@export var playback_bus: String = "Master"
-@export_range(-10.0, 10.0, 0.1) var playback_volume_db: float =  0.0
-@export_range(0.1, 2.0, 0.1) var playback_min_pitch: float = 1.0
-@export_range(0.1, 2.0, 0.1) var playback_max_pitch: float = 1.0
-@export_range(0.0, 3.0, 0.1) var playback_panning_strength: float = 1.0
+@export_group("Playback")
+@export var playback_bus: String = "sfx"
+## Limit the number of simultaneous streams of a particular key
+@export_range(1, 16, 1) var polyphony_max: int = 1
+@export_range(-10.0, 10.0, 0.5) var volume_db: float =  0.0
+@export_range(0.1, 2.0, 0.1) var min_pitch: float = 1.0
+@export_range(0.1, 4.0, 0.1) var max_pitch: float = 1.0
+@export_range(0.0, 3.0, 0.1) var panning_strength: float = 1.0
